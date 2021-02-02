d91195b4e358a452988453ef0440f93169765792
Miguel <mromerop42@gmail.com>
2021-02-01 22:52:28 -0500

    creados multiples cosas

--- imprime_diccionario.py
+++ imprime_diccionario.py
@@ -1,6 +1,11 @@
+dic_selector = {
+    '1': "id",
+    '2': "nombre",
+}
+
 diccionario = {
-    'primero': "valor1",
-    'segundo': "valor2",
+    'id': "valor1",
+    'nombre': "valor2",
 }
 
 
@@ -57,18 +62,48 @@ def f_segundo():
     #     print(i)
 
 
+def optimizador(ds):
+    print("estado actual:")
+    for i in diccionario.values():
+        print(i)
+    print("")
+    diccionario[f'{ds}'] = input(f"escribe un valor para {ds}:")
+        
+
+
 def selector():
-    a = int(input("""elige una de las opciones:
-    1) editar PRIMERO
-    2) editar SEGUNDO
-    3) no editar nada, imprimir lo que hay
-    """))
-    if a == 1:
-        f_primero()
-    elif a == 2:
-        f_segundo()
-    elif a == 3:
-        pass
+    """Aqui se escogera un valor y este determinara la llave a editar"""
+    print("campos actuales:")
+    n = 1   
+    for i in diccionario.keys():
+        print(f"{n}) {i}")
+        n += 1
+    llave = int(input("Elige una campo a editar: "))
+    ds = dic_selector[f'{llave}']
+    print(ds)
+    d = diccionario[f'{ds}']
+    print(d)
+    print("""
+    a continuacion, optimizador
+    """)
+    optimizador(ds)
+
+    
+    # optimizador(llave)
+    
+    # # funcion antigua:
+    # a = int(input("""elige una de las opciones:
+    # 1) editar PRIMERO
+    # 2) editar SEGUNDO
+    # 3) no editar nada, imprimir lo que hay
+    # """))
+    # if a == 1:
+    #     f_primero()
+    # elif a == 2:
+    #     f_segundo()
+    # elif a == 3:
+    #     pass
+    # funcion nueva:
 
 
 def run():
